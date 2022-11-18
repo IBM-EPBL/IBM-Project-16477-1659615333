2fb045930a56cc7c4628524a64cb1b8106fad706
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2022-11-18 15:04:13 +0530

    sprint 2 updated

--- Project Development Phase/Sprint 2/app.py
+++ Project Development Phase/Sprint 2/app.py
@@ -18,30 +18,21 @@ def home():
 def login():
     return render_template('account/login.html')
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST','GET'])
 def signup():
- if request.method == 'POST':
-    try:
-        sql = "INSERT INTO users VALUES('{}','{}','{}')".format(request.form["name"],request.form["email"],request.form["password"])
-        ibm_db.exec_immediate(conn,sql)
-        return render_template('account/login.html')
-    except:
-        return render_template('account/signup.html')
- else:
-    return render_template('account/signup.html')
-        # name = request.form['name']
-         #email = request.form['email']
-         #phone = request.form['phone']
-         #password = request.form['password']
-        
-         #sql ="INSERT INTO users VALUES (?,?,?,?)"
-         #stmt = ibm_db.prepare(conn,sql)
-         #ibm_db.bind_param(stmt, 1, name)
-         #ibm_db.bind_param(stmt, 2, email)
-         #ibm_db.bind_param(stmt, 3, phone)
-         #ibm_db.bind_param(stmt, 4, password)
-         #ibm_db.execute(stmt)
-   # return render_template('signup.html')
+    if request.method == 'POST':
+        # conn = connection()
+         try:
+            sql = "INSERT INTO users VALUES('{}','{}','{}')".format(request.form["name"],request.form["email"],request.form["password"])
+            ibm_db.exec_immediate(conn,sql)
+            #flash("successfully Registered !")
+            return redirect('login')
+         except:
+            #flash("Account already exists! ")
+            return render_template('account/login.html')
+    else:
+            return render_template('account/signup.html')
+
 
 
 if __name__=='__main__':
