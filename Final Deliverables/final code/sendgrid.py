1a45be8c0b6e54ffe45f396999d9d7a5006f040e
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2022-11-26 11:00:39 +0530

    readme update

--- Final Deliverables/final code/sendgrid.py
+++ Final Deliverables/final code/sendgrid.py
@@ -4,8 +4,8 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 
 def alert(main_msg):
-   mail_from = 'admin@a.com'
-   mail_to = 'admin@a.com'
+   mail_from = 'rajeshwaran1718@gmail.com'
+   mail_to = 'rajeshwaran1718@gmail.com'
    msg = MIMEMultipart()
    msg['From'] = mail_from
    msg['To'] = mail_to
@@ -16,7 +16,7 @@ def alert(main_msg):
    try:
       server = smtplib.SMTP_SSL('smtp.sendgrid.net', 465)
       server.ehlo()
-      server.login('apikey', 'SENDGRID_APIKEY')
+      server.login('apikey', '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')
       server.sendmail(mail_from, mail_to, msg.as_string())
       server.close()
       print("Mail sent successfully!")
